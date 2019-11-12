09bef8666433137ef5ade3f4bc65f164dde5750b
- <ohsang1219@naver.com>
2019-11-12 11:18:09 +0000

    11/12

---
+++ cpp/const_function.cpp
@@ -0,0 +1,91 @@
+#include	<iostream>
+
+class Marine{
+	static int total_marine_num;
+	const static int i=0;
+
+	int hp;
+	int coord_x, coord_y;
+	bool is_dead;
+
+	const int default_damage;
+
+public:
+	Marine();
+	Marine(int, int);
+	Marine(int, int, int);
+
+	int attack() const;
+	Marine& be_attacked(int);
+	void move(int, int);
+
+	void show_status();
+	static void show_total_marine();
+	~Marine();
+};
+
+int Marine::total_marine_num = 0;
+
+void Marine::show_total_marine(){
+	std::cout << "totla Marine : " << total_marine_num << std::endl;
+}
+
+Marine::Marine()
+	: hp(50), coord_x(0), coord_y(0), default_damage(5), is_dead(false){
+	total_marine_num++;	
+}
+
+Marine::Marine(int x, int y)
+	: hp(50), coord_x(x), coord_y(y), default_damage(5), is_dead(false){
+	total_marine_num++;	
+}
+
+Marine::Marine(int x, int y, int default_damage)
+	: hp(50), coord_x(x), coord_y(y), default_damage(default_damage), is_dead(false){
+	total_marine_num++;		
+}
+
+Marine::~Marine(){
+	total_marine_num--;
+}
+
+/////////////////////////////////////////상수함수는 변수의 값을 바꿀 수 없음
+/////////////////////////////////////////상수함수 내에서는 다른 상수함수만 호출 가능
+int Marine::attack() const{
+
+	return default_damage;
+}
+
+Marine& Marine::be_attacked(int damage_earn){
+	hp -= damage_earn;
+	if(hp <= 0)
+		is_dead = true;
+	return *this;
+}
+
+void Marine::move(int x, int y){
+	coord_x = x;
+	coord_y = y;
+}
+
+void Marine::show_status(){
+	std::cout << "Location : (" << coord_x << ", " << coord_y << ")" << std::endl;
+	std::cout << "HP : " << hp << std::endl;
+	std::cout << "total Marine num : " << total_marine_num << std::endl;
+}
+
+int main(){
+	Marine marine1(2, 3, 5);
+	marine1.show_status();
+
+	Marine marine2(3, 5, 10);
+	marine2.show_status();
+
+	std::cout << std::endl << "marine1->marine2" << std::endl;
+	marine2.be_attacked(marine1.attack()).be_attacked(marine1.attack());
+
+	marine1.show_status();
+	marine2.show_status();
+
+	return 0;
+}
