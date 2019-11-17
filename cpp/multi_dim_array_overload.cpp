d498104642b81fdb38c567f55fbc67816c128534
- <ohsang1219@naver.com>
2019-11-17 13:23:37 +0000

    11/17

--- cpp/multi_dim_array_overload.cpp
+++ cpp/multi_dim_array_overload.cpp
@@ -13,11 +13,82 @@ class Array{
 	struct Address{
 		int level;
 		void* next;
-	}
+	};
 
 	Address* top;
 
 public:
+	class Iterator{
+		int* location;
+		Array* arr;
+		
+		friend Int;
+
+	public:
+		Iterator(Array* arr, int* loc = NULL) : arr(arr){
+			location = new int[arr->dim];
+			for(int i=0 ; i<arr->dim ; i++)
+				location[i] = (loc != NULL ? loc[i] : 0);
+		}
+
+		Iterator(const Iterator* itr) : arr(itr.arr){
+			location = new int[arr->dim];
+			for(int i=0 ; i<arr->dim ; i++)
+				location[i] = itr.location[i];
+		}
+
+		~Iterator(){
+			delete [] location;
+		}
+
+		Iterator& operator++(){
+			if(location[0] >= arr->size[0]) 
+				return (*this);
+
+			bool carry = false;
+			int i = arr->dim - 1;
+			
+			do{
+				location[i]++;
+				if(location[i] >= arr->size[i] && i >= 1){
+					location[i] -= arr->size[i];
+					carry = true;
+					i--;
+				}
+				else
+					carry = false;
+			}while(i>=0 && carry);
+
+			return (*this);
+		}
+
+		Iterator& operator=(const Iterator& itr){
+			arr = itr.arr;
+			location = new int[itr.arr->dim];
+			for(int i=0 ; i<arr->dim ; i++)
+				location[i] = itr.location[i];
+
+			return (*this);
+		}
+
+		Iterator& operator++(int){
+			++(*this);
+			return (*this);
+		}
+
+		bool operator!=(const Iterator& itr){
+			if(itr.arr->dim != arr->dim)
+				return true;
+			for(int i=0 ; i<arr->dim ; i++)
+				if(itr.location[i] != location[i])
+					return true;
+			return false
+		}
+
+		Int operator*();
+	};
+
+	friend Iterator;
 	Array(int dim, int* array_size) : dim(dim){
 		size = new int[dim];
 		for(int i=0 ; i<dim ; i++)
@@ -62,13 +133,13 @@ public:
 			delete_address(static_cast<Address*>(current->next) + i);
 		}
 
-		delete [] current->next;
+		delete[] current->next;
 	}
 
 	Int operator[](const int index);
 	~Array(){
 		delete_address(top);
-		delete [] size;
+		delete[] size;
 	}
 };
 
@@ -80,7 +151,7 @@ class Int{
 
 public:
 	Int(int index, int _level = 0, void* _data = NULL, Array* _array = NULL)
-		: level(_level), data(_dadta), array(_array){
+		: level(_level), data(_data), array(_array){
 		if(_level < 1 || index >= array->size[_level-1]){
 			data = NULL;
 			return;
@@ -90,9 +161,9 @@ public:
 			data = static_cast<void*>((static_cast<int*>(static_cast<Array::Address*>(data)->next) + index)
 		}
 		else{
-			data = static_cast<void*>(static_cast<Array::Address*>(static_cast<Array::Address*>(data)->next + index);
+			data = static_cast<void*>(static_cast<Array::Address*>(static_cast<Array::Address*>(data)->next) + index);
 		}
-	}
+	};
 
 	Int(const Int& i) : data(i.data), level(i.level), array(i.array){}
 
