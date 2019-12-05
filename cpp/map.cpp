bd1697687a0f5671896771986574a2337fc8ea5d
- <ohsang1219@naver.com>
2019-12-05 14:17:32 +0000

    12/5

---
+++ cpp/map.cpp
@@ -0,0 +1,38 @@
+#include	<iostream>
+#include	<map>
+#include	<string>
+
+template <typename K, typename V>
+void print_map(std::map<K, V>& m){
+	for(auto itr = m.begin() ; itr != m.end() ; itr++)
+		std::cout << itr->first << " " << itr->second << std::endl;
+}
+
+template <typename K, typename V>
+void search_and_print(std::map<K, V>& m, K key){
+	auto itr = m.find(key);
+	if(itr != m.end()){
+		std::cout << key << "->" << m[key] << std::endl;
+	}
+	else
+		std::cout << "no key in map" << std::endl;
+}
+
+int main(){
+	std::map<std::string, double> pitcher_list;
+
+	pitcher_list.insert(std::pair<std::string, double>("박세용", 2.23));
+	pitcher_list.insert(std::pair<std::string, double>("해커", 2.93));
+	pitcher_list.insert(std::pair<std::string, double>("피어밴드", 2.95));
+	pitcher_list.insert(std::make_pair("차우찬", 3.04));
+	pitcher_list.insert(std::make_pair("장원준", 3.05));
+	pitcher_list.insert(std::make_pair("헥터", 3.09));
+
+	pitcher_list["니퍼트"] = 3.56;
+	pitcher_list["박종훈"] = 3.76;
+	pitcher_list["켈리"] = 3.90;
+
+	print_map(pitcher_list);
+	search_and_print(pitcher_list, std::string("니퍼트"));
+	search_and_print(pitcher_list, std::string("류현진"));
+}
