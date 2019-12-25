78e31b98becebfbd0fbf9ec1218b9cc503c1143a
- <ohsang1219@naver.com>
2019-12-25 14:01:13 +0000

    12/25

---
+++ cpp/PointTemplate.h
@@ -0,0 +1,15 @@
+#ifndef __POINT_TEMPLATE_H
+#define __POINT_TEMPLATE_H
+
+template <typename T>
+class Point{
+private:
+	T xpos;
+	T ypos;
+
+public:
+	Point(T x=0, T y=0);
+	void ShowPosition() const;
+};
+
+#endif
